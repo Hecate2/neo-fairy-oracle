fb10d06e6ae5a3b9769213e4ceec93bede078737
Hecate2 <2474101468@qq.com>
2022-07-07 12:12:56 +0800

    contract to be tested

---
+++ oracle-demo/oracle-demo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Numerics;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Attributes;
+using Neo.SmartContract.Framework.Native;
+using Neo.SmartContract.Framework.Services;
+
+namespace demo
+{
+    [DisplayName("Oracle Demo")]
+    [ManifestExtra("Author", "Neo")]
+    [ManifestExtra("Email", "dev@neo.org")]
+    [ManifestExtra("Description", "This is a Oracle using template")]
+    public class OracleDemo : SmartContract
+    {
+        static readonly string PreData = "RequestData";
+        static readonly string PreCount = "RequestCount";
+
+        public static string GetRequstData()
+        {
+            return Storage.Get(Storage.CurrentContext, PreData);
+        }
+
+        public static BigInteger GetRequstCount()
+        {
+            return (BigInteger)Storage.Get(Storage.CurrentContext, PreCount);
+        }
+
+        public static void CreateRequest(string url, string filter, string callback, byte[] userData, long gasForResponse)
+        {
+            Oracle.Request(url, filter, callback, userData, gasForResponse);
+        }
+
+        public static void Callback(string url, byte[] userData, int code, byte[] result)
+        {
+            if (Runtime.CallingScriptHash != Oracle.Hash) throw new Exception("Unauthorized!");
+            StorageContext currentContext = Storage.CurrentContext;
+            Storage.Put(currentContext, PreData, (ByteString)result);
+            Storage.Put(currentContext, PreCount, (BigInteger)Storage.Get(currentContext, PreCount) + 1);
+        }
+    }
+}
